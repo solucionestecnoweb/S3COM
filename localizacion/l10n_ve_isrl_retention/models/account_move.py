38833a2d4c8d51e25a01efbcb995782a6513938e
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-06-08 13:18:09 -0400

    testeo islr v1

--- localizacion/l10n_ve_isrl_retention/models/account_move.py
+++ localizacion/l10n_ve_isrl_retention/models/account_move.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import api, fields, models,_
 from odoo.exceptions import ValidationError
 from odoo.exceptions import UserError
 import base64
