d63d1e307068659b8cd8b19ab08d59d77d654075
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-06-08 15:27:58 -0400

    testeo islr v7

--- localizacion/l10n_ve_isrl_retention/wizard/islr_resume_wizard.py
+++ localizacion/l10n_ve_isrl_retention/wizard/islr_resume_wizard.py
@@ -151,7 +151,7 @@ class IslrResumeWizard(models.TransientModel):
             WHERE islr.isrl_date BETWEEN '%s' AND '%s'
             AND islr.move_type in ('in_invoice', 'in_refund')
             AND islr.state = 'done'
-            AND r.code = '055'
+            AND r.code = r.code  # ojo
             AND r.people_type = 'domi_ledal_entity'
         """ % (desde, hasta)
         self._cr.execute(query)
