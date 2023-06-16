81ba23882385fc7b0301c923963107fb83ca10e4
subone <subone@MacBook-Air-de-subone.local>
2023-06-16 08:29:35 -0400

    ADD: traslate report account move

---
+++ localizacion/custom_report_acc_move/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Custom Report Account",
+    "version": "15.0",
+    "summary": "Custom Report Account",
+    "author": "Carlos Fernandez",
+    "license": "AGPL-3",
+    "depends": ["base", "account_reports", "account"],
+    "data": [
+       
+
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+
+}
